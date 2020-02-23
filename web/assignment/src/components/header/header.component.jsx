2e53bc89c4c91a68d508fed927e84146942c508c
Sanket Nerella <sanket.nerella@gmail.com>
2020-02-24 00:39:35 +0530

    React Initial setup

---
+++ web/assignment/src/components/header/header.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+
+const Header = () => {
+    return (
+        <div>
+            Header works!
+        </div>
+    );
+}
+
+export default Header;
---
+++ web/assignment/src/components/home-item/home-item.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomeItem = () => {
+    return (
+        <div>
+            Home Item Works!!
+        </div>
+    );
+}
+
+export default HomeItem;
