321e76337d9b65579a7cfad4bb6d674dba76dfd5
Sanket Nerella <sanket.nerella@gmail.com>
2020-02-23 23:05:30 +0530

    Basic Routes setup completed

--- svc/controllers/tasksController.js
+++ svc/controllers/tasksController.js
@@ -5,6 +5,31 @@ const getAllTasks = function(){
     return axios.get(`${config.api.baseUrl}/tasks`);
 }
 
+const getTaskById = function(id){
+    return axios.get(`${config.api.baseUrl}/tasks/${id}`);
+}
+
+const createTask = function(body){
+    return axios.post(`${config.api.baseUrl}/tasks`, body);
+}
+
+const createSubTask = function(body, id){
+    return axios.post(`${config.api.baseUrl}/tasks/${id}`, body);
+}
+
+const editTask = function(body, id){
+    return axios.put(`${config.api.baseUrl}/tasks/${id}`, body);
+}
+
+const deleteTask = function(id){
+    return axios.delete(`${config.api.baseUrl}/tasks/${id}`);
+}
+
 module.exports = {
-    getAllTasks: getAllTasks
+    getAllTasks: getAllTasks,
+    getTaskById: getTaskById,
+    createTask: createTask,
+    createSubTask: createSubTask,
+    editTask: editTask,
+    deleteTask: deleteTask
 }
--- svc/routes/router.js
+++ svc/routes/router.js
@@ -2,20 +2,67 @@ const log = require('../utils/logger');
 const tasksController = require('../controllers/tasksController');
 
 const registerRoutes = function(server){
-    // 
+    // Get All Tasks
     server.get('/tasks', function(request, response, next){
         log.info("Get Tasks :: Request Initiated");
         tasksController.getAllTasks().then(function(tasksResponse){
             log.info("Log success", tasksResponse.data);
-            const customResponse = {
-                "results": tasksResponse.data
-            }
             response.send(tasksResponse.data);
         }).catch(function(error){
-            log.info("Log Error");
+            log.info("Get All Tasks Error");
         });
         return next();
-    })
+    });
+
+    // Get Task By Id
+    server.get('/tasks/:id', function(request, response, next){
+        tasksController.getTaskById(request.params.id).then(function(tasksResponse){
+            response.send(tasksResponse.data);
+        }).catch(function(error){
+            log.info("Get Task with Id Error!");
+        })
+        return next();
+    });
+
+    // Create Root Task
+    server.post('/tasks', function(request, response, next){
+        tasksController.createTask(request.body).then(function(taskResponse){
+            response.send(taskResponse.data);
+        }).catch(function(error){
+            log.info("Create Task Error!");
+        });
+        return next();
+    });
+
+    // Create Sub Tasks
+    server.post('/tasks/:id', function(request, response, next){
+        tasksController.createSubTask(request.body, request.params.id).then(function(taskResponse){
+            response.send(taskResponse.data);
+        }).catch(function(error){
+            log.info("Create Sub Tasks Error!");
+        });
+        return next();
+    });
+
+    //Edit task
+    server.put('/tasks/:id', function(request, response, next){
+        tasksController.editTask(request.body, request.params.id).then(function(taskResponse){
+            response.send(taskResponse.data);
+        }).catch(function(error){
+            log.info("Edit Task Error!");
+        });
+        return next();
+    });
+
+    // Delete Task
+    server.del('/tasks/:id', function(request, response, next){
+        tasksController.deleteTask(request.params.id).then(function(taskResponse){
+            response.send(taskResponse.data);
+        }).catch(function(error){
+            log.info("Delete Task Error!");
+        });
+        return next();
+    });
 }
 
 module.exports = registerRoutes;
